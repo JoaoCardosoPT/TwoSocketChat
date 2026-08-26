e53852c8b5223ed6b4eb1a00b493a1bca6b43c0f
Joao <joaopaulocardosokls@gmail.com>
2026-08-26 15:58:39 +0100

    Chat improvement and error fixed

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
+<<<<<<< HEAD
 # TwoSocket Chat
 
 This project was built to understand the fundamentals of networking in C, including sockets, binding, listening for connections, accepting clients and more
+=======
+# TwoSockeChat
 
+It is a simple two-client TCP chat server written in C.
+>>>>>>> a126ca3 (Chat improvement and error fixed)
+
+## How It Works
+
+The server listens for connections on:
+
+- Address: `127.0.0.1`
+- Port: `8080`
+- Protocol: TCP
+- Address family: IPv4
+
+The server waits for exactly two clients to connect.
+
+When one client sends a message, the server receives it and forwards it to the other client.
 
--- src/chat.c
+++ src/chat.c
@@ -1,12 +1,14 @@
 #include <arpa/inet.h>
 #include <err.h>
+#include <errno.h>
 #include <netinet/in.h>
+#include <poll.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <poll.h>
-#include <errno.h>
+#include <unistd.h>
 
 #include "../include/chat.h"
 
@@ -23,67 +25,80 @@ void start_chat(int server_fd) {
   client1 = accept(server_fd, (struct sockaddr *)&client_adrr, &client_len);
 
   if (client1 == -1) {
-    err(EXIT_FAILURE, "accept");
+    err(EXIT_FAILURE, "accept \n");
   }
 
-  printf("client 1 connected");
+  printf("client 1 connected \n");
+
   client2 = accept(server_fd, (struct sockaddr *)&client_adrr, &client_len);
 
   if (client2 == -1) {
     err(EXIT_FAILURE, "accept");
   }
 
-  printf("client 2 connected");
-  
+  printf("client 2 connected \n");
+
   // fds stores the sockets tha poll needs to whatch
   fds[0].fd = client1;
   fds[0].events = POLLIN;
   fds[0].revents = 0;
-  
+
   fds[1].fd = client2;
   fds[1].events = POLLIN;
   fds[1].revents = 0;
-  
+
   // Number of users && wait indefenetly
   nfds_t nfds = 2;
   int timeout = -1;
 
   while (1) {
-    printf("Waiting for responses");
+    printf("Waiting for responses \n");
 
     int rc = poll(fds, nfds, timeout);
 
     if (rc == -1) {
-      // errno is used to report and identify the specific reaseon why a system or a library failed 
+      // errno is used to report and identify the specific reaseon why a system
+      // or a library failed
       if (errno == EINTR) {
         continue;
       }
-      
+
       perror("poll");
       break;
     }
-  }
- 
-  //type to represent quantitys of files descriptors used for pool
-  for (nfds_t i = 0; i < nfds; i++) {
-    if (fds[i].revents && POLLIN) {
-      printf("Client %zu as data Available\n", i + 1);
 
-      /*
-      TO DO - use recv in fds
-      find the other client
-      use send to send the data
-      */
+    // type to represent quantitys of files descriptors used for pool
+    for (nfds_t i = 0; i < nfds; i++) {
+      char buffer[1024];
+      if (fds[i].revents & POLLIN) {
+        printf("Client %d as data Available\n", i + 1);
+        int destination = 1 - i;
+
+        // the -1 means the recv reads 1023 bytes and lets the last byte free
+        ssize_t received = recv(fds[i].fd, buffer, sizeof(buffer) - 1, 0);
+
+        if (received > 0) {
+          send(fds[destination].fd, buffer, received, 0);
+        } else if (received == 0) {
+          printf("Client %d disconnected", i + 1);
+          close(client1);
+          close(client2);
+
+          return;
+        } else {
+          err(EXIT_FAILURE, "Data Error");
+        }
+      }
+
+      if (fds[i].revents & POLL_HUP) {
+        printf("Client %d as disconected \n", i + 1);
+        break;
+      }
+
+      if (fds[i].revents & (POLL_ERR | POLLNVAL)) {
+        printf("Error on client %d \n", i + 1);
+        break;
+      }
     }
-
-  if (fds[i].revents && POLL_HUP) {
-      printf("Client %zu as disconected \n", i + 1);     
-      break; 
   }
-
-  if (fds[i].revents && (POLL_ERR | POLLNVAL)) {
-      printf("Error on client %zu \n", i + 1);     
-      break; 
-  }
-
 }
